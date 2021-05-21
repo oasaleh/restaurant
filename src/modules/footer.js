b5f13a2d96751f7c9867188f934be746faa3e77a
Omar <omarasaleh@hotmail.com>
2021-05-20 22:09:17 -0500

    Fix social medial icons links

--- src/modules/footer.js
+++ src/modules/footer.js
@@ -2,7 +2,7 @@ import createTag from './utilities';
 
 const renderFooter = () => {
   const footerSection = createTag('footer', 'footerSection');
-  const socialMediaContactText = `<ul class="socialmedia list"> <li class="socialmedia item"> <img class="social icon" id="iconFacebook" src="/src/resources/photos/icons/facebook-icon.svg" /> </li> <li class="socialmedia item"> <img class="social icon" id="iconTwitter" src="/src/resources/photos/icons/twitter-icon.svg" /> </li> <li class="socialmedia item"> <img class="social icon" id="iconInstagram" src="/src/resources/photos/icons/instagram-icon.svg" /> </li> </ul>`;
+  const socialMediaContactText = `<ul class="socialmedia list"> <li class="socialmedia item"> <img class="social icon" id="iconFacebook" src="resources/photos/icons/facebook-icon.svg" /> </li> <li class="socialmedia item"> <img class="social icon" id="iconTwitter" src="resources/photos/icons/twitter-icon.svg" /> </li> <li class="socialmedia item"> <img class="social icon" id="iconInstagram" src="resources/photos/icons/instagram-icon.svg" /> </li> </ul>`;
   const socialMediaContact = createTag('section', 'socialMediaContact');
   socialMediaContact.innerHTML = socialMediaContactText;
   const rightsText = `<div><p>All Rights Reserved 2021</p></div>`;
