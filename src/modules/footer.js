9cb212e135df66f0cdeb0c7ebacdfbd9bd826569
Omar <omarasaleh@hotmail.com>
2021-05-30 22:50:14 -0500

    Restored links in the footer section

--- src/modules/footer.js
+++ src/modules/footer.js
@@ -2,7 +2,7 @@ import createTag from './utilities';
 
 const renderFooter = () => {
   const footerSection = createTag('footer', 'footerSection');
-  const socialMediaContactText = `<ul class="socialmedia list"> <li class="socialmedia item"> <object class="social icon" id="iconFacebook" data="resources/photos/icons/facebook-icon.svg" type="image/svg+xml"></object> </li> <li class="socialmedia item"> <img class="social icon" id="iconTwitter" src="resources/photos/icons/twitter-icon.svg" /> </li> <li class="socialmedia item"> <img class="social icon" id="iconInstagram" src="resources/photos/icons/instagram-icon.svg" /> </li> </ul>`;
+  const socialMediaContactText = `<ul class="socialmedia list"> <li class="socialmedia item"> <img src="resources/photos/icons/facebook-icon.png" class="social icon" id="iconFacebook" ></img> </li> <li class="socialmedia item"> <img class="social icon" id="iconTwitter" src="resources/photos/icons/twitter-icon.svg" /> </li> <li class="socialmedia item"> <img class="social icon" id="iconInstagram" src="resources/photos/icons/instagram-icon.svg" /> </li> </ul>`;
   const socialMediaContact = createTag('section', 'socialMediaContact');
   socialMediaContact.innerHTML = socialMediaContactText;
   const rightsText = `<div><p>All Rights Reserved 2021</p></div>`;
